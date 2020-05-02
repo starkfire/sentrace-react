7629ad99196fe1f1e46d2e6abd6fe285146fc41c
starkfire <kingbeyondthenorth@gmail.com>
2020-05-02 11:46:01 +0800

    add: enableBluetooth and getBluetoothAdapter

--- android/app/src/main/java/com/sentracereact/TracerModule.java
+++ android/app/src/main/java/com/sentracereact/TracerModule.java
@@ -7,10 +7,27 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+
+import android.util.Log;
+
 class TracerModule extends ReactContextBaseJavaModule {
-    
+
+    private static final int REQUEST_ENABLE_BT = 539;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothManager bluetoothManager;
     private Context context;
     private ReactApplicationContext reactContext;
 
@@ -24,6 +41,16 @@ class TracerModule extends ReactContextBaseJavaModule {
         return "TracerModule";
     }
 
+    // Bluetooth Adapter
+    private BluetoothAdapter getBluetoothAdapter() {
+        if (bluetoothAdapter == null) {
+            BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+            bluetoothAdapter = manager.getAdapter();
+        }
+
+        return bluetoothAdapter;
+    }
+
     // Check for BLE Support
     @ReactMethod
     public void checkBleSupport(Callback callback) {
@@ -34,5 +61,24 @@ class TracerModule extends ReactContextBaseJavaModule {
         }
 
         callback.invoke(bleSupported);
+    }
+
+    // Enable Bluetooth
+    @ReactMethod
+    public void enableBluetooth(Callback callback) {
+        if (getBluetoothAdapter() == null) {
+            callback.invoke("No Bluetooth Found on Device");
+            return;
+        }
+
+        if (!getBluetoothAdapter().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (getCurrentActivity() == null)
+                callback.invoke("Current Activity Not Available");
+            else
+                getCurrentActivity().startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            callback.invoke();
+        }
     }
 }
