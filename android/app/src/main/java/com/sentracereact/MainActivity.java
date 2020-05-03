bf8a93db38862467b5155f9d91ff2174d654a0f0
starkfire <kingbeyondthenorth@gmail.com>
2020-05-03 09:22:49 +0800

    add splash screen

--- android/app/src/main/java/com/sentracereact/MainActivity.java
+++ android/app/src/main/java/com/sentracereact/MainActivity.java
@@ -2,6 +2,9 @@ package com.sentracereact;
 
 import com.facebook.react.ReactActivity;
 
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,5 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SentraceReact";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
